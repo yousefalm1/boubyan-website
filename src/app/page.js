5a74468f2e856b636029ceed154cd2aeb63fa2b7
unknown <osama.albader99@gmail.com>
2024-10-27 14:03:13 +0300

    adding footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Footer from "./components/footer/page.js";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-      <h1>Hello</h1>
+    <main className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 absolute inset-0">
+      <Footer />
     </main>
   );
 }
