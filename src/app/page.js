b8a50200e6ad06c4a6a724802af9fb6375e63362
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-10-27 12:57:53 +0300

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import HeroPage from "./components/main-content1/page";
+import HeroPage from "./components/hero/page";
 
 export default function Home() {
   return (
